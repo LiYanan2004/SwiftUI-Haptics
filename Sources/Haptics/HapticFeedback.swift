3f150a46e01b5ebb40324dc28c0062c52b0e94da
LiYanan2004 <37542129+LiYanan2004@users.noreply.github.com>
2024-02-03 19:20:30 +0800

    Add more documentations

--- Sources/Haptics/HapticFeedback.swift
+++ Sources/Haptics/HapticFeedback.swift
@@ -4,6 +4,9 @@ protocol Haptic: Sendable {
     @MainActor var performHapticFeedback: @Sendable () async -> Void { get }
 }
 
+/// Represents a type of haptic and/or audio feedback that can be played.
+///
+/// This feedback can be passed to `View.hapticFeedback` to play it.
 public struct HapticFeedback: Haptic {
     internal var label: String
     internal var performHapticFeedback: @Sendable () async -> Void
@@ -13,6 +16,7 @@ public struct HapticFeedback: Haptic {
         self.performHapticFeedback = performHapticFeedback
     }
     
+    /// Perform feedback in Main Actor.
     func perform() {
         Task { @MainActor in
             await self.performHapticFeedback()
@@ -21,10 +25,16 @@ public struct HapticFeedback: Haptic {
 }
 
 extension HapticFeedback {
+    /// The flexibility to be represented by a type of feedback.
+    ///
+    /// Flexibility values can be passed to `HapticFeedback.impact(flexibility:intensity:)`.
     public enum Flexibility: Int, Sendable {
         case rigid, solid, soft
     }
     
+    /// The weight to be represented by a type of feedback.
+    ///
+    /// Weight values can be passed to `HapticFeedback.impact(weight:intensity:)`.
     public enum Weight: Int, Sendable {
         case light, medium, heavy
     }
